8f5bda010957aeb13c1dfe8880ba7b166bd9b1d4
Lucas <lukasrub@gmail.com>
2016-05-11 13:45:31 -0300

    No significant changes

--- postHTTPgetResponse.py
+++ postHTTPgetResponse.py
@@ -1,19 +1,20 @@
 import json
 import urllib2
 #JSON with the data we want to send
-data = {'first_name':   'name',
-    'last_name': 'lastName',
-    'email':  'email',
-    'phone': 'mobile',
-    'urls': [	'URL1', 
-    			'URL2',
-    			'URL3'
+data = {
+    'first_name':   "name",
+    'last_name': "lastName",
+    'email':  "email",
+    'phone': "999999999",
+    'cover_letter': "letter",
+    'urls': [	"URL1", 
+    			"URL2",
+    			"URL3"
     		]}
 data_json = json.dumps(data)
-url = 'URL'
+url = 'targetURL'
 r = urllib2.Request(url)
 r.add_header('Content-Type', 'application/json')
-
 response = urllib2.urlopen(r, data_json)
 print response.getcode()
 print response.read()
